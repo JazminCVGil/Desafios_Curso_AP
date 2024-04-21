d6382d8088f053e70a7df84daf00ca757d504890
JazminCVGil <jazmincvgil2007@gmail.com>
2024-04-21 11:42:14 -0300

    desafio 2

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let producto = "collar de perlas";
+let precio_u = 9500;
+let cantidad 
+alert("Buenos dias, estas a punto de comprar un " + producto + "\n cuyo precio es de $" + precio_u);
+
+let cantidad_deseada = prompt("Por favor, ingrese con valores numericos la cantidad de productos que desea comprar");
+
+const total_a_pagar = precio_u * cantidad_deseada
+
+alert("Usted ha comprado" + cantidad_deseada + " " + producto + "\n y su total a pagar es de $" + total_a_pagar)
